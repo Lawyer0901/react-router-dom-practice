5bd941ed2ab6fe26c7ad7f836baeb6a5df19c06f
Lawyer0901 <lawyer0901@gmail.com>
2023-11-30 23:19:49 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="https://lawyer0901.github.io/react-router-dom-practice/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
